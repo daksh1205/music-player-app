3ec3d3b9f08f564890824de166d7274da14385dd
daksh <dakshyt1205@gmail.com>
2024-01-16 17:35:04 +0530

    update: playing music

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -53,30 +53,41 @@ class Home extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: ListView.builder(
                 physics: const BouncingScrollPhysics(),
-                itemCount: 100,
+                itemCount: snapshot.data!.length,
                 itemBuilder: (BuildContext context, int index) {
                   return Container(
                     margin: const EdgeInsets.only(bottom: 4),
-                    child: ListTile(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
+                    child: Obx(
+                      () => ListTile(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        tileColor: bgColor,
+                        title: Text(
+                          snapshot.data![index].displayNameWOExt,
+                          style: ourStyle(family: bold, size: 15),
+                        ),
+                        subtitle: Text(
+                          "${snapshot.data![index].artist}",
+                          style: ourStyle(family: regular, size: 12),
+                        ),
+                        leading: QueryArtworkWidget(
+                          id: snapshot.data![index].id,
+                          type: ArtworkType.AUDIO,
+                          nullArtworkWidget: const Icon(
+                            Icons.music_note,
+                            color: whiteColor,
+                            size: 32,
+                          ),
+                        ),
+                        trailing: controller.playIndex.value == index
+                            ? const Icon(Icons.play_arrow,
+                                color: whiteColor, size: 26)
+                            : null,
+                        onTap: () {
+                          controller.playSong(snapshot.data![index].uri, index);
+                        },
                       ),
-                      tileColor: bgColor,
-                      title: Text(
-                        snapshot.data![index].displayNameWOExt,
-                        style: ourStyle(family: bold, size: 15),
-                      ),
-                      subtitle: Text(
-                        "${snapshot.data![index].artist}",
-                        style: ourStyle(family: regular, size: 12),
-                      ),
-                      leading: const Icon(
-                        Icons.music_note,
-                        color: whiteColor,
-                        size: 32,
-                      ),
-                      trailing: const Icon(Icons.play_arrow,
-                          color: whiteColor, size: 26),
                     ),
                   );
                 },
